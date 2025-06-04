49e338605939519459315eeee2bde48b41a6bed3
MAITUMELO RADIPODI <mradipodi@gmail.com>
2025-06-04 09:36:54 +0200

    updates

--- dist/index.js
+++ dist/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:8080",
+    origin: "https://mmele.vercel.app",
     credentials: true,
 }));
 console.log("4");
--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({  
-    origin: "http://localhost:8080",
+    origin: "https://mmele.vercel.app",
     credentials: true,
     
 }))
