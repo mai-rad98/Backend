120919f3e81e6e3d470e14b3bdd700c659513a69
MAITUMELO RADIPODI <mradipodi@gmail.com>
2025-06-14 10:34:55 +0200

    fix : updated dist folder

--- dist/routes/gamesRoutes.js
+++ dist/routes/gamesRoutes.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import express from 'express';
-import { createGameInvite, respondToGameInvite, getGameDetails, makeMove, endGame, getUserGames, getUsers } from '../controllers/gamesController.js';
+import { createGameInvite, respondToGameInvite, getGameDetails, makeMove, endGame, getUserGames, getUsers,forfeitGame } from '../controllers/gamesController.js';
 import { protectRoute } from '../middleware/authMiddleware.js';
 const router = express.Router();
 router.get("/user", protectRoute, getUsers);
@@ -9,5 +9,6 @@ router.post('/invite/:gameId/respond', protectRoute, respondToGameInvite);
 router.get('/:gameId', protectRoute, getGameDetails);
 router.post('/:gameId/move', protectRoute, makeMove);
 router.post('/:gameId/leave', protectRoute, endGame);
+router.post('/:gameId/forfeit', protectRoute, forfeitGame);
 router.get('/', protectRoute, getUserGames);
 export default router;
